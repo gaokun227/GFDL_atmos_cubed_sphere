79bc993a5c08d54c16edf2d84e9a78c30d9f8901
Flexible Modeling System <Flexible.Modeling.System@noaa.gov>
2007-03-19 00:00:00 -0400

    Import of nalanda.

---
+++ driver/solo/atmosphere.F90
@@ -0,0 +1,269 @@
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!                                                                   !!
+!!                   GNU General Public License                      !!
+!!                                                                   !!
+!! This file is part of the Flexible Modeling System (FMS).          !!
+!!                                                                   !!
+!! FMS is free software; you can redistribute it and/or modify       !!
+!! it and are expected to follow the terms of the GNU General Public !!
+!! License as published by the Free Software Foundation.             !!
+!!                                                                   !!
+!! FMS is distributed in the hope that it will be useful,            !!
+!! but WITHOUT ANY WARRANTY; without even the implied warranty of    !!
+!! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the     !!
+!! GNU General Public License for more details.                      !!
+!!                                                                   !!
+!! You should have received a copy of the GNU General Public License !!
+!! along with FMS; if not, write to:                                 !!
+!!          Free Software Foundation, Inc.                           !!
+!!          59 Temple Place, Suite 330                               !!
+!!          Boston, MA  02111-1307  USA                              !!
+!! or see:                                                           !!
+!!          http://www.gnu.org/licenses/gpl.txt                      !!
+!!                                                                   !!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+module atmosphere_mod
+
+!-----------------------------------------------------------------------
+!
+!    interface for FV dynamical core with Held-Suarez forcing
+!
+!-----------------------------------------------------------------------
+
+
+use constants_mod, only: grav, kappa, cp_air, pi
+use fms_mod,       only: file_exist, open_namelist_file,   &
+                         error_mesg, FATAL,                &
+                         check_nml_error, stdlog, stdout,  &
+                         write_version_number,             &
+                         close_file, set_domain, nullify_domain, mpp_pe, mpp_root_pe
+use time_manager_mod, only: time_type, get_time, set_time, operator(+)
+use mpp_domains_mod,  only: domain2d
+
+!------------------
+! FV specific codes:
+!------------------
+use fv_arrays_mod, only: fv_atmos_type
+use fv_pack_mod,   only: fv_init, domain, fv_end, adiabatic
+use fv_phys_mod,   only: fv_phys, fv_nudge
+use fv_diagnostics_mod, only: fv_diag_init, fv_diag, fv_time
+use timingModule,   only: timing_on, timing_off
+use fv_restart_mod, only: fv_restart
+use fv_dynamics_mod, only: fv_dynamics
+
+!-----------------------------------------------------------------------
+
+implicit none
+private
+
+public   atmosphere_init, atmosphere,  atmosphere_end, atmosphere_domain
+
+!-----------------------------------------------------------------------
+
+character(len=128) :: version = '$Id: atmosphere.F90,v 14.0 2007/03/15 21:57:53 fms Exp $'
+character(len=128) :: tag = '$Name: nalanda $'
+
+!-----------------------------------------------------------------------
+!---- private data ----
+
+type        (time_type) :: Time_step_atmos
+real                    :: dt_atmos
+integer :: sec
+integer days, seconds
+
+logical :: cold_start      = .false.       ! read in initial condition
+
+type(fv_atmos_type), allocatable :: Atm(:)
+!-----------------------------------------------------------------------
+
+contains
+
+!#######################################################################
+
+  subroutine atmosphere_init ( Time_init, Time, Time_step )
+
+    type (time_type), intent(in) :: Time_step
+    type (time_type), intent(in) :: Time_init
+    type (time_type), intent(in) :: Time
+
+    ! local:
+    integer :: axes(4)
+    integer :: ss, ds
+    integer :: ntiles=1
+    integer i,j, isc, iec, jsc, jec
+    real pp(2)
+
+
+  !----- write version and namelist to log file -----
+
+    call write_version_number ( version, tag )
+
+  !---- compute physics/atmos time step in seconds ----
+
+    Time_step_atmos = Time_step
+    call get_time (Time_step_atmos, sec)
+    dt_atmos = real(sec)
+
+  !----- initialize FV dynamical core -----
+!   cold_start = (.not.file_exist('INPUT/fv_rst.res.nc').and. .not.file_exist('INPUT/'//fms_tracers_file))
+    cold_start = (.not.file_exist('INPUT/fv_core.res.nc'))
+
+    allocate(Atm(ntiles))
+    call fv_init(Atm(:),dt_atmos)  ! allocates Atm components
+
+    ! Init model data
+         call timing_on('fv_restart')
+    call fv_restart(domain, Atm, dt_atmos, seconds, days, cold_start)
+         call timing_off('fv_restart')
+
+#ifdef PERTURB_IC
+    isc = Atm(1)%isc
+    iec = Atm(1)%iec
+    jsc = Atm(1)%jsc
+    jec = Atm(1)%jec
+#ifdef SW_DYNAMICS
+! TEST CASE-7
+!     if ( .not. cold_start ) then
+         do j=jsc,jec
+            do i=isc,iec
+               pp(1) = Atm(1)%agrid(i,j,1)
+               pp(2) = Atm(1)%agrid(i,j,2)
+               Atm(1)%delp(i,j,1) = Atm(1)%delp(i,j,1) + 120.*grav*cos(pp(2)) *  &
+               exp( -(3.*(pp(1)-pi))**2 ) * exp( -(15.*(pp(2)-pi/4.))**2 )
+            enddo
+         enddo
+!     endif
+#endif
+#endif
+
+! Need to implement time in fv_restart files
+!   if ( .not. cold_start ) then 
+! !
+! ! Check consistency in Time
+! !
+!     fv_time = set_time (seconds, days)
+!     call get_time (Time, ss,  ds)
+!
+!     if(seconds /= ss .or. days /= ds) then
+!       write(stdout(),*) 'FMS:', ds, ss
+!       write(stdout(),*) 'FV:', days, seconds
+!       call error_mesg('FV_init:','Time inconsistent between fv_rst and INPUT/atmos_model.res',FATAL)
+!     endif
+!   else
+      fv_time = time
+!   endif
+
+
+    Atm(1)%full_phys = .false.
+
+    call fv_diag_init(Atm, axes, Time, Atm(1)%npx, Atm(1)%npy, Atm(1)%npz)
+
+!   if( nlev > 1 ) call hs_forcing_init ( axes, Time )
+
+!-----------------------------------------------------------------------
+
+  end subroutine atmosphere_init
+
+
+!#######################################################################
+
+  subroutine atmosphere (Time)
+    type(time_type), intent(in) :: Time
+
+    real:: zvir = 0.           ! !!! need to be changed
+    real:: time_total
+    real:: tau_winds, tau_press, tau_temp
+
+#ifdef NUDGE_IC
+    tau_winds =  1. * 3600.
+    tau_press = -1.
+    tau_temp  = -1.
+#else
+    tau_winds = -1.
+    tau_press = -1.
+    tau_temp  = -1.
+#endif
+
+    fv_time = Time + Time_step_atmos
+    call get_time (fv_time, seconds,  days)
+
+    time_total = days*86400. + seconds
+
+    if ( tau_winds>0. .or. tau_press>0. .or. tau_temp>0. )     &
+    call  fv_nudge(Atm(1)%npz, Atm(1)%isc, Atm(1)%iec, Atm(1)%jsc, Atm(1)%jec, Atm(1)%ng, &
+                   Atm(1)%u, Atm(1)%v, Atm(1)%delp, Atm(1)%pt, dt_atmos,    &
+                   tau_winds, tau_press, tau_temp)
+
+  !---- call fv dynamics -----
+    zvir = 0.         ! no virtual effect if not full physics
+
+    call timing_on('fv_dynamics')
+    call set_domain(Atm(1)%domain)  ! needed for diagnostic output done in fv_dynamics
+    call fv_dynamics(Atm(1)%npx, Atm(1)%npy, Atm(1)%npz, Atm(1)%ncnst, Atm(1)%ng,   & 
+                     dt_atmos, Atm(1)%consv_te, Atm(1)%fill, Atm(1)%reproduce_sum, kappa,   &
+                     cp_air, zvir, Atm(1)%ks, Atm(1)%ncnst, Atm(1)%n_split, Atm(1)%q_split, &
+                     Atm(1)%u, Atm(1)%v, Atm(1)%w, Atm(1)%delz,                             &
+                     Atm(1)%hydrostatic, Atm(1)%pt, Atm(1)%delp, Atm(1)%q, Atm(1)%ps,       &
+                     Atm(1)%pe, Atm(1)%pk, Atm(1)%peln, Atm(1)%pkz,                         &
+                     Atm(1)%phis, Atm(1)%omga, Atm(1)%ua, Atm(1)%va, Atm(1)%uc, Atm(1)%vc,  &
+                     Atm(1)%ak, Atm(1)%bk, Atm(1)%mfx, Atm(1)%mfy, Atm(1)%cx, Atm(1)%cy,    &
+                     Atm(1)%u_srf, Atm(1)%v_srf, Atm(1)%srf_init, Atm(1)%ze0,               &
+                     Atm(1)%hybrid_z, time_total)
+    call nullify_domain()
+    call timing_off('fv_dynamics')
+
+    if(Atm(1)%npz /=1 .and. .not. adiabatic)then
+
+                                                         call timing_on('FV_PHYS')
+       call fv_phys(Atm(1)%npx, Atm(1)%npy, Atm(1)%npz, Atm(1)%isc, Atm(1)%iec,  &
+                    Atm(1)%jsc, Atm(1)%jec, Atm(1)%ng, Atm(1)%ncnst,             &
+                    Atm(1)%u, Atm(1)%v, Atm(1)%pt, Atm(1)%q, Atm(1)%pe,          &
+                    Atm(1)%delp, Atm(1)%peln, Atm(1)%pkz, dt_atmos,              &
+                    Atm(1)%ua, Atm(1)%va, Atm(1)%phis, Atm(1)%agrid,             &
+                    Atm(1)%ak, Atm(1)%bk, Atm(1)%ks, Atm(1)%ps, Atm(1)%pk,       &
+                    Atm(1)%u_srf, Atm(1)%v_srf, Atm(1)%delz,                     &
+                    Atm(1)%hydrostatic, Atm(1)%oro, .true., .false.,             &
+                    (mpp_pe()==mpp_root_pe()), Atm(1)%do_Held_Suarez,            &
+                    fv_time, time_total)
+                                                        call timing_off('FV_PHYS')
+    endif
+
+  !---- diagnostics for FV dynamics -----
+
+    call timing_on('FV_DIAG')
+
+    call fv_diag(Atm, zvir, fv_time, Atm(1)%print_freq)
+
+    call timing_off('FV_DIAG')
+
+  end subroutine atmosphere
+
+
+ subroutine atmosphere_end
+
+!----- initialize domains for writing global physics data -----
+
+!   call set_domain ( domain )
+    call get_time (fv_time, seconds,  days)
+!   call write_fv_rst( 'RESTART/fv_rst.res', days, seconds, grav, &
+!        restart_format )
+
+    call fv_end(Atm)
+
+    deallocate(Atm)
+
+  end subroutine atmosphere_end
+
+ subroutine atmosphere_domain ( fv_domain )
+ type(domain2d), intent(out) :: fv_domain
+
+!  returns the domain2d variable associated with the coupling grid
+!  note: coupling is done using the mass/temperature grid with no halos
+
+   fv_domain = domain
+
+ end subroutine atmosphere_domain
+
+
+end module atmosphere_mod
