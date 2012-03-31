adb0a1d3cb1b85b8516ba367dc1eeb1f87c418a3
Flexible Modeling System <Flexible.Modeling.System@noaa.gov>
2012-03-31 00:00:00 -0400

    Import of siena_201203.

--- driver/solo/hswf.F90
+++ driver/solo/hswf.F90
@@ -29,7 +29,7 @@ module hswf_mod
 
 !---- version number -----
       character(len=128) :: version = '$Id: hswf.F90,v 19.0 2012/01/06 19:56:28 fms Exp $'
-      character(len=128) :: tagname = '$Name: siena_201202 $'
+      character(len=128) :: tagname = '$Name: siena_201203 $'
 
 contains
 
