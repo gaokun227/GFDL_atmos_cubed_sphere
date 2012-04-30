5ddee5388ff82c8f73955284976007ecd28c4f76
Flexible Modeling System <Flexible.Modeling.System@noaa.gov>
2012-04-30 00:00:00 -0400

    Import of siena_201204.

--- driver/solo/fv_phys.F90
+++ driver/solo/fv_phys.F90
@@ -18,7 +18,7 @@ public :: fv_phys, fv_nudge
 
 !---- version number -----
   character(len=128) :: version = '$Id: fv_phys.F90,v 19.0 2012/01/06 19:56:26 fms Exp $'
-  character(len=128) :: tagname = '$Name: siena_201203 $'
+  character(len=128) :: tagname = '$Name: siena_201204 $'
 
 contains
 
