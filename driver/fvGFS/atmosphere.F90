06c319990cd136a0b9dcd5906ffcf8f02d7a6731
Rusty Benson <rusty.benson@noaa.gov>
2018-01-09 14:47:14 -0500

    updates needed to FV3 to support internal namelist read for nesting
    removed fdiag and first_time_step from FV3 and moved it to atmos_model.F90
    
    added an API for the nggps dynamic core diagnostics

--- driver/fvGFS/atmosphere.F90
+++ driver/fvGFS/atmosphere.F90
@@ -89,6 +89,7 @@ public :: atmosphere_resolution, atmosphere_grid_bdry, &
           atmosphere_hgt, atmosphere_scalar_field_halo, &
 !rab          atmosphere_tracer_postinit, &
 ! atmosphere_diss_est, &
+          atmosphere_nggps_diag, &
           get_bottom_mass, get_bottom_wind,   &
           get_stock_pe, set_atmosphere_pelist
 
@@ -99,7 +100,7 @@ public :: atmos_phys_driver_statein
 ! version number of this module
 ! Include variable "version" to be written to log file.
 #include<file_version.h>
-character(len=7)   :: mod_name = 'fvGFS/atmosphere_mod'
+character(len=20)   :: mod_name = 'fvGFS/atmosphere_mod'
 
 !---- private data ----
   type (time_type) :: Time_step_atmos
@@ -254,7 +255,6 @@ contains
 !----- initialize atmos_axes and fv_dynamics diagnostics
        !I've had trouble getting this to work with multiple grids at a time; worth revisiting?
    call fv_diag_init(Atm(mytile:mytile), Atm(mytile)%atmos_axes, Time, npx, npy, npz, Atm(mytile)%flagstruct%p_ref)
-   call fv_nggps_diag_init(Atm(mytile:mytile), Atm(mytile)%atmos_axes, Time)
 
 !---------- reference profile -----------
     ps1 = 101325.
@@ -751,6 +751,33 @@ contains
  end subroutine atmosphere_scalar_field_halo
 
 
+ subroutine atmosphere_nggps_diag (Time, init)
+   !----------------------------------------------
+   ! api for output of NCEP/EMC diagnostics
+   ! 
+   ! if register is present and set to .true.
+   ! will make the initialization call
+   !
+   ! outputs 3D state fields via either
+   ! NCEP write_component or GFDL/FMS diag_manager
+   !----------------------------------------------
+   type(time_type),   intent(in) :: Time
+   logical, optional, intent(in) :: init
+
+   if (PRESENT(init)) then
+     if (init == .true.) then
+       call fv_nggps_diag_init(Atm(mytile:mytile), Atm(mytile)%atmos_axes, Time)
+       return
+     else
+       call mpp_error(FATAL, 'atmosphere_nggps_diag - calling with init present, but set to .false.')
+     endif
+   endif
+
+   call fv_nggps_diag(Atm(mytile:mytile), zvir, Time)
+
+ end subroutine atmosphere_nggps_diag
+
+
 !--- Need to know the formulation of the mixing ratio being imported into FV3
 !--- in order to adjust it in a consistent manner for advection
 !rab subroutine atmosphere_tracer_postinit (IPD_Data, Atm_block)
@@ -1083,14 +1110,6 @@ contains
      call timing_on('FV_DIAG')
      call fv_diag(Atm(mytile:mytile), zvir, fv_time, Atm(mytile)%flagstruct%print_freq)
      first_diag = .false.
-
-     fv_time = Time_next - Atm(n)%Time_init
-     call get_time (fv_time, seconds,  days)
-    !--- perform diagnostics on GFS fdiag schedule
-     if (ANY(nint(Atm(mytile)%fdiag(:)*3600.) == (days*24*3600+seconds)) .or. (days*24*3600+seconds) == dt_atmos ) then
-       if (mpp_pe() == mpp_root_pe()) write(6,*) 'NGGPS:FV3 DIAG STEP', (real(days)*24. + real(seconds)/3600.)
-       call fv_nggps_diag(Atm(mytile:mytile), zvir, Time_next)
-     endif
      call timing_off('FV_DIAG')
 
      call mpp_clock_end(id_fv_diag)
