c302461901c6027b37100ec1f89cf5cd325bdf2c
Linjiong Zhou <Linjiong.Zhou@noaa.gov>
2022-11-22 14:41:44 -0500

    Add the option to disable intermediate physics.
    Note that cloud fraction will be zero at the first time step.

--- model/fv_arrays.F90
+++ model/fv_arrays.F90
@@ -365,6 +365,7 @@ module fv_arrays_mod
                                     !< schemes. Also requires GFDL microphysics be installed within the physics driver.
    logical :: consv_checker = .false.!< turn on energy and water conservation checker
    logical :: do_fast_phys = .false.!< Controls fast physics, in which the SA-TKE-EDMF and part of the GWD are 
+   logical :: do_intermediate_phys = .true.!< Controls fast physics, in which the SA-TKE-EDMF and part of the GWD are 
                                     !< within the acoustic time step of FV3. If .true. disabling the SA-TKE-EDMF 
                                     !< and part of the GWD in the intermediate physics.
    logical :: do_inline_mp = .false.!< Controls Inline GFDL cloud microphysics, in which the full microphysics is
