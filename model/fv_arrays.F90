96ee1a0da039e75b2ed7d9cd06748027d5440227
Linjiong Zhou <Linjiong.Zhou@noaa.gov>
2023-03-06 11:50:51 -0500

    Update of thermodynamics in the PBL scheme.

--- model/fv_arrays.F90
+++ model/fv_arrays.F90
@@ -1154,7 +1154,6 @@ module fv_arrays_mod
     real, _ALLOCATABLE :: dqgsfc(:,:)     _NULL
     real, _ALLOCATABLE :: dusfc(:,:)     _NULL
     real, _ALLOCATABLE :: dvsfc(:,:)     _NULL
-    real, _ALLOCATABLE :: dksfc(:,:)     _NULL
     real, _ALLOCATABLE :: fast_te_a_chg(:,:)     _NULL
     real, _ALLOCATABLE :: fast_tw_a_chg(:,:)     _NULL
     real, _ALLOCATABLE :: fast_te_b_chg(:,:)     _NULL
@@ -1725,7 +1724,6 @@ contains
        allocate ( Atm%inline_pbl%dqgsfc(is:ie,js:je) )
        allocate ( Atm%inline_pbl%dusfc(is:ie,js:je) )
        allocate ( Atm%inline_pbl%dvsfc(is:ie,js:je) )
-       allocate ( Atm%inline_pbl%dksfc(is:ie,js:je) )
        if (Atm%flagstruct%consv_checker) then
           allocate ( Atm%inline_pbl%fast_te_a_chg(is:ie,js:je) )
           allocate ( Atm%inline_pbl%fast_tw_a_chg(is:ie,js:je) )
@@ -1931,7 +1929,6 @@ contains
               Atm%inline_pbl%dqgsfc(i,j) = real_big
               Atm%inline_pbl%dusfc(i,j) = real_big
               Atm%inline_pbl%dvsfc(i,j) = real_big
-              Atm%inline_pbl%dksfc(i,j) = real_big
            enddo
         enddo
         if (Atm%flagstruct%consv_checker) then
@@ -2319,7 +2316,6 @@ contains
        deallocate ( Atm%inline_pbl%dqgsfc )
        deallocate ( Atm%inline_pbl%dusfc )
        deallocate ( Atm%inline_pbl%dvsfc )
-       deallocate ( Atm%inline_pbl%dksfc )
        if (Atm%flagstruct%consv_checker) then
           deallocate ( Atm%inline_pbl%fast_te_a_chg )
           deallocate ( Atm%inline_pbl%fast_tw_a_chg )
